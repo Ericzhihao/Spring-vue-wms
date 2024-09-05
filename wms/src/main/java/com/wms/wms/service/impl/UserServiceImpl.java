0cb5561cdc372732ffae791d4e7052f696e38100
EricZhihao <zhihao.yu19@gmail.com>
2024-09-05 07:07:41 -0700

    first commit

---
+++ wms/src/main/java/com/wms/wms/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.wms.wms.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wms.wms.service.UserService;
+
+import java.security.Provider;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+    
+}
